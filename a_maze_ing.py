20f220d752d65302c4fff0fcd18bcb4b4f0978de
Maurice Lorenz <me@mauricelorenz.com>
2026-03-13 16:56:19 +0100

    Implement config file parsing

---
+++ a_maze_ing.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from sys import exit
+from typing import List, Dict, Callable
+
+def parse_value(config_dict: Dict, key: str, conversion: str) -> None:
+    try:
+        if conversion == "int":
+            config_dict[key] = int(config_dict[key])
+        elif conversion == "tuple":
+            temp: List = config_dict[key].split(",")
+            temp_int: List = [int(nbr) for nbr in temp]
+            config_dict[key] = tuple(temp_int)
+        elif conversion == "bool":
+            if config_dict[key] == "True":
+                config_dict[key] = True
+            elif config_dict[key] == "False":
+                config_dict[key] = False
+            else:
+                raise Exception("Invalid string for bool!")
+    except Exception as e:
+        print(f"Error: {e}")
+        exit()
+
+
+def parse_config() -> Dict:
+    file: str = "config.txt"
+    config_list: List = []
+    try:
+        with open(file) as f:
+            for line in f:
+                if not line.startswith("#") and line.strip():
+                    config_list.append(line.strip())
+    except FileNotFoundError:
+        print(f"File {file} not found. Please make sure it exists!")
+        exit()
+    config_dict: Dict = {}
+    try:
+        for item in config_list:
+            split_item: List = item.split("=")
+            if len(split_item) != 2:
+                raise Exception("Invalid key value pairs!")
+            config_dict[split_item[0]] = split_item[1]
+    except Exception as e:
+        print(f"Error: {e}")
+        exit()
+    parse_value(config_dict, "WIDTH", "int")
+    parse_value(config_dict, "HEIGHT", "int")
+    parse_value(config_dict, "ENTRY", "tuple")
+    parse_value(config_dict, "EXIT", "tuple")
+    parse_value(config_dict, "PERFECT", "bool")
+    return config_dict
+
+
+def main() -> None:
+    config_dict: Dict = parse_config()
+
+
+if __name__ == "__main__":
+    main()
