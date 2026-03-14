9c14d6226d3648473ec87648dcb12bf1c742b694
lelero17 <leandro-emmerling@web.de>
2026-03-14 13:21:36 +0100

    Move MazeGenerator to seperate module maze.py

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,6 +2,7 @@
 
 from sys import argv, exit
 from typing import List, Dict
+from maze import MazeGenerator
 
 
 def parse_value(config_dict: Dict, key: str, conversion: str) -> None:
@@ -82,45 +83,6 @@ def validate_config(config_dict: Dict) -> bool:
     if not config_dict["OUTPUT_FILE"]:
         return False
     return True
-
-
-BLOCKED = -1
-FOUR: List[tuple] = [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (2, 3), (2, 4)]
-TWO: List[tuple] = [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (1, 2), (0, 2),
-                    (0, 3), (0, 4), (1, 4), (2, 4)]
-
-
-class MazeGenerator:
-    """Geneates a maze using the backtracker algorithm."""
-
-    def __init__(self, width: int, height: int) -> None:
-        """Initialize the MazeGenerator with a given width and height.
-            grid: 2D list of integers representing the maze cells.
-                  0 = normal cell, -1 = blocked cell.
-        """
-
-        self.width: int = width
-        self.height: int = height
-        self.grid: List[List[int]] = []
-        for y in range(height):
-            row: List[int] = []
-            for x in range(width):
-                row.append(0)
-            self.grid.append(row)
-
-    def place_pattern(self) -> None:
-        """Place the 42 pattern and set these cells as BLOCKED (-1)."""
-
-        if self.width < 11 or self.height < 9:
-            print("Maze too small to place 42 pattern!")
-            return
-        
-        start_x: int = (self.width // 2) - 3
-        start_y: int = (self.height // 2) - 2
-        for (px, py) in FOUR:
-            self.grid[start_y + py][start_x + px] = BLOCKED
-        for (px, py) in TWO:
-            self.grid[start_y + py][start_x + px + 4] = BLOCKED
 
 
 def main() -> None:
