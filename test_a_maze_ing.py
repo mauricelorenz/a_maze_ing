7d3b53ffd0394666fe250abd6d7cb3727358901e
Maurice Lorenz <me@mauricelorenz.com>
2026-03-15 23:37:57 +0100

    Fix type annotations in tests

--- test_a_maze_ing.py
+++ test_a_maze_ing.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 from os import remove
 from pytest import raises
 from a_maze_ing import parse_config, validate_config, parse_value
@@ -52,214 +53,214 @@ PERFECT=True
 
 
 def test_validate_config_happy() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 0),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert validate_config(d)
 
 
 def test_validate_config_width_neg() -> None:
-    d = {"WIDTH": -20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 0),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": -20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_height_neg() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": -15,
-         "ENTRY": (0, 0),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": -15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_entry_x_low() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (-100, 0),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (-100, 0),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_entry_x_high() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (100, 0),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (100, 0),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_entry_y_low() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, -100),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, -100),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_entry_y_high() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 100),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 100),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_exit_x_low() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 0),
-         "EXIT": (-100, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (-100, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_exit_x_high() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 0),
-         "EXIT": (100, 14),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (100, 14),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_exit_y_low() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 0),
-         "EXIT": (19, -100),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (19, -100),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_exit_y_high() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 0),
-         "EXIT": (19, 100),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (19, 100),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_entry_eq_exit() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (10, 10),
-         "EXIT": (10, 10),
-         "OUTPUT_FILE": "maze.txt",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (10, 10),
+                         "EXIT": (10, 10),
+                         "OUTPUT_FILE": "maze.txt",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_validate_config_empty_outfile() -> None:
-    d = {"WIDTH": 20,
-         "HEIGHT": 15,
-         "ENTRY": (0, 0),
-         "EXIT": (19, 14),
-         "OUTPUT_FILE": "",
-         "PERFECT": True}
+    d: Dict[str, Any] = {"WIDTH": 20,
+                         "HEIGHT": 15,
+                         "ENTRY": (0, 0),
+                         "EXIT": (19, 14),
+                         "OUTPUT_FILE": "",
+                         "PERFECT": True}
     assert not validate_config(d)
 
 
 def test_parse_value_str_happy() -> None:
-    d = {"OUTPUT_FILE": "maze.txt"}
+    d: Dict[str, Any] = {"OUTPUT_FILE": "maze.txt"}
     parse_value(d, "OUTPUT_FILE", "str")
     assert d["OUTPUT_FILE"] == "maze.txt"
 
 
 def test_parse_value_str_key_error() -> None:
-    d = {"OUTPUT_FILE": "maze.txt"}
+    d: Dict[str, Any] = {"OUTPUT_FILE": "maze.txt"}
     with raises(SystemExit):
         parse_value(d, "FOO", "str")
 
 
 def test_parse_value_int_happy() -> None:
-    d = {"WIDTH": "1"}
+    d: Dict[str, Any] = {"WIDTH": "1"}
     parse_value(d, "WIDTH", "int")
     assert d["WIDTH"] == 1
 
 
 def test_parse_value_int_key_error() -> None:
-    d = {"WIDTH": "1"}
+    d: Dict[str, Any] = {"WIDTH": "1"}
     with raises(SystemExit):
         parse_value(d, "FOO", "int")
 
 
 def test_parse_value_int_value_error() -> None:
-    d = {"WIDTH": "abc"}
+    d: Dict[str, Any] = {"WIDTH": "abc"}
     with raises(SystemExit):
         parse_value(d, "WIDTH", "int")
 
 
 def test_parse_value_tuple_happy() -> None:
-    d = {"ENTRY": "0,0"}
+    d: Dict[str, Any] = {"ENTRY": "0,0"}
     parse_value(d, "ENTRY", "tuple")
     assert d["ENTRY"] == (0, 0)
 
 
 def test_parse_value_tuple_key_error() -> None:
-    d = {"ENTRY": "0,0"}
+    d: Dict[str, Any] = {"ENTRY": "0,0"}
     with raises(SystemExit):
         parse_value(d, "FOO", "tuple")
 
 
 def test_parse_value_tuple_alpha() -> None:
-    d = {"ENTRY": "1,abc"}
+    d: Dict[str, Any] = {"ENTRY": "1,abc"}
     with raises(SystemExit):
         parse_value(d, "ENTRY", "tuple")
 
 
 def test_parse_value_tuple_too_few() -> None:
-    d = {"ENTRY": "1"}
+    d: Dict[str, Any] = {"ENTRY": "1"}
     with raises(SystemExit):
         parse_value(d, "ENTRY", "tuple")
 
 
 def test_parse_value_tuple_too_many() -> None:
-    d = {"ENTRY": "1,2,3"}
+    d: Dict[str, Any] = {"ENTRY": "1,2,3"}
     with raises(SystemExit):
         parse_value(d, "ENTRY", "tuple")
 
 
 def test_parse_value_bool_true() -> None:
-    d = {"PERFECT": "True"}
+    d: Dict[str, Any] = {"PERFECT": "True"}
     parse_value(d, "PERFECT", "bool")
     assert d["PERFECT"] is True
 
 
 def test_parse_value_bool_false() -> None:
-    d = {"PERFECT": "False"}
+    d: Dict[str, Any] = {"PERFECT": "False"}
     parse_value(d, "PERFECT", "bool")
     assert d["PERFECT"] is False
 
 
 def test_parse_value_bool_key_error() -> None:
-    d = {"PERFECT": "True"}
+    d: Dict[str, Any] = {"PERFECT": "True"}
     with raises(SystemExit):
         parse_value(d, "FOO", "bool")
 
 
 def test_parse_value_bool_foo() -> None:
-    d = {"PERFECT": "Foo"}
+    d: Dict[str, Any] = {"PERFECT": "Foo"}
     with raises(SystemExit):
         parse_value(d, "PERFECT", "bool")
