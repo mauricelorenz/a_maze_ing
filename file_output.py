678a8dfd79190b16b31c474f7a4fc85a8f2f01e0
Maurice Lorenz <me@mauricelorenz.com>
2026-03-23 17:04:14 +0100

    Finalize refactoring of create_output_file()

--- file_output.py
+++ file_output.py
@@ -11,16 +11,16 @@ def create_output_file(maze: MazeGenerator) -> None:
         config_dict: Dict containing parsed config values.
         path: List of directions as strings(N, E, S, W).
     """
-    output_string = ""
+    output_string: str = ""
     for row in maze.grid:
         for col in row:
             output_string += f"{col:X}"
         output_string += "\n"
-    output_string += f"\n{maze.config['ENTRY'][0]},{maze.config['ENTRY'][1]}\n"
-    output_string += f"{maze.config['EXIT'][0]},{maze.config['EXIT'][1]}\n"
-    output_string += "".join(maze.path) + "\n"
+    output_string += f"\n{maze.entry[0]},{maze.entry[1]}\n"
+    output_string += f"{maze.exit[0]},{maze.exit[1]}\n"
+    output_string += f"{''.join(maze.path)}\n"
     try:
-        with open(maze.config["OUTPUT_FILE"], "w") as f:
+        with open(maze.output_file, "w") as f:
             f.write(output_string)
     except Exception as e:
-        print(f"Error while creating {maze.config['OUTPUT_FILE']}: {e}")
+        print(f"Error while creating {maze.output_file}: {e}")
