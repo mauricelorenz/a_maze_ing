d72f511d5e3a9dad097d68136aa3d49fe4f51923
Leandro Emmerling <lemmerli@student.42heilbronn.de>
2026-03-30 17:06:22 +0200

    Add type hints to and change output color in render_image()

--- bonus_image_renderer.py
+++ bonus_image_renderer.py
@@ -12,15 +12,16 @@ def render_image(maze: MazeGenerator, rendered_maze: List[List[int]]) -> None:
         maze: MazeGenerator instance containing the grid.
         rendered_maze: 2D list containing the pixel representation.
     """
-    canvas_width = (len(rendered_maze[0]) + 2) * 20
-    canvas_height = (len(rendered_maze) + 2) * 20
+    canvas_width: int = (len(rendered_maze[0]) + 2) * 20
+    canvas_height: int = (len(rendered_maze) + 2) * 20
     pixel_color: Dict[int, Tuple[int, int, int]] = {1: (0, 0, 0),
-                                                    2: (0, 255, 0),
-                                                    3: (255, 0, 0),
-                                                    4: (0, 0, 255),
-                                                    5: (255, 255, 0)}
-    img = Image.new("RGB", (canvas_width, canvas_height), (255, 255, 255))
-    draw = ImageDraw.Draw(img)
+                                                    2: (0, 128, 0),
+                                                    3: (196, 0, 0),
+                                                    4: (36, 114, 200),
+                                                    5: (229, 229, 16)}
+    img: Image.Image = Image.new("RGB", (canvas_width, canvas_height),
+                                 (255, 255, 255))
+    draw: ImageDraw.ImageDraw = ImageDraw.Draw(img)
     for r, row in enumerate(rendered_maze):
         for c, col in enumerate(row):
             top_left: Tuple[int, int] = (c * 20 + 20, r * 20 + 20)
